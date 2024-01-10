d5cc93360ce66871ffa5b8b3c0a551a9b633d238
TheSameHenry <ulti1235@gmail.com>
2024-01-10 20:45:35 +0200

    more models

---
+++ Scripts/Environment/door.gd
@@ -0,0 +1,20 @@
+extends Node3D
+
+
+@onready var rotation_point = $rotation_point
+
+
+func _ready():
+	rotation_point.set_rotation_degrees(Vector3(0, 0, 0))
+
+
+func open():
+	rotation_point.set_rotation_degrees(Vector3(0, 90, 0))
+	
+
+func close():
+	rotation_point.set_rotation_degrees(Vector3(0, 0, 0))
+	
+
+func _process(delta):
+	pass
