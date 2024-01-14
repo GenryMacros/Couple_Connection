ca22123486afc0de41a3a7b1fa5b7d62a62df200
alhaffthemage@gmail.com <alhaffthemage@gmail.com>
2024-01-14 05:43:49 +0200

    Added new dialog. Some minor changes

--- Scripts/NotesAndInteractions/ExclamationMark.gd
+++ Scripts/NotesAndInteractions/ExclamationMark.gd
@@ -5,10 +5,11 @@ extends Sprite3D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.texture = view_port.get_texture();
-	var camera_pos = GameManager.player.camera_point.global_position;
-	var pos = labelPos.global_position - camera_pos;  
-	var dw = max(abs(pos.x), absf(pos.z))/8;
-	pixel_size *= 1 + (dw);
+	if GameManager.player != null:
+		var camera_pos = GameManager.player.camera_point.global_position;
+		var pos = labelPos.global_position - camera_pos;  
+		var dw = max(abs(pos.x), absf(pos.z))/8;
+		pixel_size *= 1 + (dw);
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
