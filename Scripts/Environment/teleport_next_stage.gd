3caddf682e293fe5b76c978a2522cf74d308a77c
TheSameHenry <ulti1235@gmail.com>
2024-01-14 05:36:33 +0200

    fixed scene to scene transition

--- Scripts/Environment/teleport_next_stage.gd
+++ Scripts/Environment/teleport_next_stage.gd
@@ -1,7 +1,6 @@
 extends Area3D
 
 
-func _on_body_entered(body: Node3D):
-	print("Entered teleport")
+func _on_area_entered(area):
 	var loading_screen = load("res://UI/loading_scene.tscn"); 
 	get_tree().change_scene_to_packed(loading_screen);
--- game_manager.gd
+++ game_manager.gd
@@ -4,6 +4,7 @@ var nextScene
 var scenes = ["res://Game_levels/main.tscn",
 			  "res://Game_Levels/dungeon.tscn"]
 var player: Node
+var current_scene = 0
 var is_game_paused = false
 
 
@@ -14,9 +15,8 @@ func set_player(player_node):
 	player = player_node
 
 func set_new_scene():
-	for scene in scenes:
-		print(scene)
-	scenes.pop_front()
-	if !scenes.is_empty():
-		nextScene = scenes[0]
+	current_scene += 1
+	if current_scene >= len(scenes):
+		current_scene = 0
+	nextScene = scenes[current_scene]
  
